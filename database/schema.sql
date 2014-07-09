05a23be4a728906c9d37ec8c83666a56c8a0dbf1
sakins@GS.DOI.NET <sakins@GS.DOI.NET@a6478e1e-ec95-11dc-b814-c3d4bc21ddcb>
2014-07-09 21:09:32 +0000

    First cut at all tables
    git-svn-id: http://subversion.wr.usgs.gov/repos/web/budget-proposals@6489 a6478e1e-ec95-11dc-b814-c3d4bc21ddcb

--- database/schema.sql
+++ database/schema.sql
@@ -43,3 +43,60 @@ CREATE TABLE fbmsaccounts (
   accountno VARCHAR(128),
   proposalid INTEGER references proposals(proposalid)
 );
+
+CREATE TABLE conferences (
+  conferenceid SERIAL Primary Key,
+  meeting VARCHAR(256),
+  location VARCHAR(128)
+);
+
+CREATE TABLE conferencerates (
+  conferencerateid SERIAL Primary Key,
+  conferenceid INTEGER references conferences(conferenceid),
+  effectivedate TIMESTAMP,
+  perdiem REAL,
+  registration REAL,
+  groundtransport REAL,
+  airfare REAL
+);
+
+CREATE TABLE conferenceattendee (
+  conferenceattendeeid SERIAL Primary Key,
+  conferenceid INTEGER references conferences(conferenceid),
+  proposalid INTEGER references proposals(proposalid),
+  peopleid INTEGER references people(peopleid),
+  meetingdays INTEGER,
+  traveldays INTEGER
+);
+
+CREATE TABLE tasks (
+  taskid BIGSERIAL Primary Key,
+  proposalid INTEGER references proposals(proposalid),
+  taskname VARCHAR(1024)
+);
+
+CREATE TABLE staffing (
+  staffingid BIGSERIAL Primary Key,
+  taskid BIGINT references tasks(taskid),
+  peopleid INTEGER references people(peopleid),
+  fiscalyear VARCHAR(4),
+  q1hours REAL,
+  q2hours REAL,
+  q3hours REAL,
+  q4hours REAL
+  flexhours REAL
+);
+
+CREATE TABLE expensetypes (
+  expensetypeid SERIAL Primary Key,
+  description VARCHAR(256)
+);
+
+CREATE TABLE expenses (
+  expenseid BIGSERIAL Primary Key,
+  proposalid INTEGER references proposals(proposalid),
+  expensetypeid INTEGER references expensetypes(expensetypeid),
+  description VARCHAR(256),
+  amount REAL,
+  fiscalyear VARCHAR(4)
+);

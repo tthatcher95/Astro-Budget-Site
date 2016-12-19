f0c6fb5708375cf3c2dd9713d060540cc26e6603
sakins@GS.DOI.NET <sakins@GS.DOI.NET@a6478e1e-ec95-11dc-b814-c3d4bc21ddcb>
2016-12-19 19:10:36 +0000

    initial checkin
    git-svn-id: http://subversion.wr.usgs.gov/repos/web/budget-proposals@7324 a6478e1e-ec95-11dc-b814-c3d4bc21ddcb

---
+++ htdocs/js/date-tools.js
@@ -0,0 +1,32 @@
+function updateDropdown (id) {
+  var newDate = $("#" + id).datepicker("getDate");
+
+  console.log ("datepicker returned " + newDate);
+  if (newDate == null) {
+    console.log ("newDate is NULL!!!!!");
+    newDate = new Date();
+    $("#" + id).val((newDate.getMonth() + 1) + '/' + newDate.getDate() + '/' +  newDate.getFullYear());
+  }
+
+  var month = newDate.getMonth() + 1;
+  var year  = newDate.getFullYear();
+
+  var fyYear = year - 2000;
+  if (month >= 10) {
+    fyYear += 1;
+  }
+
+  var FYDate = '10/01/20' + (fyYear - 1);
+  var hid = '#' + id + 'dd';
+
+  console.log ("Setting " + hid + " to " + FYDate);
+  $(hid).val(FYDate);
+}
+
+function updateCalendar (id) {
+  var hid = '#' + id;
+  var idname = hid + 'dd';
+  var newDate = $(idname).val();
+ 
+  $(hid).val (newDate);
+}
